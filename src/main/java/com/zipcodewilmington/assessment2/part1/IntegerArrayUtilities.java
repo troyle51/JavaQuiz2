39606a3221581167c972210f366e365120e1aba6
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-07-15 16:29:32 -0400

    Initial commit

---
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -0,0 +1,19 @@
+package com.zipcodewilmington.assessment2.part1;
+
+public class IntegerArrayUtilities {
+    public Boolean hasEvenLength(Integer[] array) {
+        return null;
+    }
+
+    public Integer[] range(int start, int stop) {
+        return null;
+    }
+
+    public Integer getSumOfFirstTwo(Integer[] array) {
+        return null;
+    }
+
+    public Integer getProductOfFirstTwo(Integer[] array) {
+        return null;
+    }
+}
---
+++ src/test/java/com/zipcodewilmington/assessment2/part2/arrayutility/MergeTest.java
@@ -0,0 +1,74 @@
+package com.zipcodewilmington.assessment2.part2.arrayutility;
+
+import com.zipcodewilmington.assessment2.TestUtils;
+import com.zipcodewilmington.assessment2.part2.ArrayUtility;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class MergeTest {
+    private ArrayUtility utility;
+
+    @Before
+    public void setup() {
+        utility = new ArrayUtility();
+    }
+
+    @Test
+    public void test1() {
+        //Given
+        Integer[] array1 = {9};
+        Integer[] array2 = {6};
+        Integer[] expected = {9, 6};
+
+        //When
+        test(array1, array2, expected);
+    }
+
+    @Test
+    public void test2() {
+        //Given
+        Integer[] array1 = {5, 1, 8};
+        Integer[] array2 = {4, 2};
+        Integer[] expected = {5, 1, 8, 4, 2};
+
+        // when
+        test(array1, array2, expected);
+    }
+
+
+
+    @Test
+    public void test3() {
+        //Given
+        Integer[] array1 = {5, 1, 8, 9, 10};
+        Integer[] array2 = {4, 2, 11};
+        Integer[] expected = {9, 10, 11, 5, 1, 8, 4, 2};
+
+        // when
+        test(array1, array2, expected);
+    }
+
+
+    @Test
+    public void test4() {
+        //Given
+        Integer[] array1 = {1993, 1994, 1995};
+        Integer[] array2 = {2003, 2004, 2005};
+        Integer[] expected = {1993, 1994, 1995, 2003, 2004, 2005};
+
+        // when
+        test(array1, array2, expected);
+    }
+
+
+    private void test(Integer[] array1, Integer[] array2, Integer[] expected) {
+        // when
+        Integer[] actual = utility.merge(array1, array2);
+
+        // then
+        TestUtils.assertArrayEquals(expected, actual);
+    }
+}
