33876d93d6e432714561dde573a03f48902f89f9
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-16 07:14:33 -0400

    basicUtils

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -2,18 +2,18 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        return (value >= 5) ? true : false;
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+        return (value <= 7) ? true : false;
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        return (valueToEvaluate >= 5 && valueToEvaluate <= 7) ? true : false;
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+        return (string.toLowerCase().charAt(0) == character || string.toUpperCase().charAt(0) == character) ? true : false;
     }
 }
